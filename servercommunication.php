cf6ec4ae1ca0a5c081adeb4585fcf77e80e83727
JonasDreessen <dreessenjonas95@gmail.com>
2019-07-30 16:26:45 +0200

    redirect page after submitting form

--- servercommunication.php
+++ servercommunication.php
@@ -3,11 +3,14 @@
 $htmlFirstName = $_GET["firstname"];
 $htmlLastName = $_GET["lastname"];
 $htmlMessage = $_GET["textmessage"];
+
 $myPDO = new PDO('mysql:host=localhost;dbname=Api_exercise_becode', 'root', 'root');
 $result = $myPDO->query("INSERT INTO user_information (first_name, last_name, text_message)
     VALUES ('$htmlFirstName', '$htmlLastName', '$htmlMessage')");
     
+    
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-?>
+
+    header( 'Location: index.html' );
