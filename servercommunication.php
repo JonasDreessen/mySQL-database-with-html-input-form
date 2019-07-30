efe6935e516d0c152d468a50e6572132b3550843
JonasDreessen <dreessenjonas95@gmail.com>
2019-07-30 15:53:04 +0200

    added html form and working php

---
+++ servercommunication.php
@@ -0,0 +1,13 @@
+<?php
+
+$htmlFirstName = $_GET["firstname"];
+$htmlLastName = $_GET["lastname"];
+$htmlMessage = $_GET["textmessage"];
+$myPDO = new PDO('mysql:host=localhost;dbname=Api_exercise_becode', 'root', 'root');
+$result = $myPDO->query("INSERT INTO user_information (first_name, last_name, text_message)
+    VALUES ('$htmlFirstName', '$htmlLastName', '$htmlMessage')");
+    
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+?>
