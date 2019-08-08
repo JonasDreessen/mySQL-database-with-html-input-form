c207a72a4bab0b64d259ea267f0e9a4b2ce5552e
JonasDreessen <49682725+JonasDreessen@users.noreply.github.com>
2019-08-08 13:56:48 +0200

    Update sql2.php

--- sql2.php
+++ sql2.php
@@ -1,11 +1,1 @@
-if($_POST['submitupdate']){
-        if (empty($updateInputMessage)){
-            echo "type in the message you would like to update";
-        }else if(empty($updateTitle)){ 
-            echo "type in the title you would like to update";
-        } else{ 
-            $stmtupdate = $pdo->prepare("UPDATE notes SET message_text=? WHERE title=?"); 
-            $stmtupdate->execute([$updateInputMessage, $updateTitle]); 
-            echo "Records was updated successfully."; 
-    }
-}
+"UPDATE notes SET message_text=? WHERE title=?" 
